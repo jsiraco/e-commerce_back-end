e3341f84d41b66d25423c1b4947d112cbaa0145b
Jas <ragnarock.josef@gmail.com>
2021-09-27 14:38:26 -0400

    product and category models completed

--- models/Product.js
+++ models/Product.js
@@ -4,12 +4,49 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        customValidation(value) {
+          const regex = /^(\d*\.)?\d+$/igm;
+          if (!regex.test(value));
+          throw new Error("Please use alphanumeric characters only");
+        }
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isAlphanumeric: true,
+      },
+    },
+    category_id: {
+      type:DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+        unique: false
+      }
+    }
   },
   {
     sequelize,
